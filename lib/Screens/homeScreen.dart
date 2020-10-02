175e24a46be6e3cf6ce11c7cde8b98fd7ae2ec7d
RomjanHossain <romjanhossain726526@gmail.com>
2020-10-03 00:02:03 +0600

    updated

--- lib/Screens/homeScreen.dart
+++ lib/Screens/homeScreen.dart
@@ -1,132 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:knowyourbook/Screens/swapTrand/bookmarks.dart';
+import 'package:knowyourbook/Screens/swapTrand/books.dart';
+import 'package:knowyourbook/Screens/swapTrand/profile.dart';
+import 'package:knowyourbook/Screens/swapTrand/topGrowsing.dart';
 import 'package:knowyourbook/Widgets/bottomNavBar.dart';
-import 'package:knowyourbook/Widgets/myBtn.dart';
 import 'package:line_icons/line_icons.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  static List<Widget> myPages = [
+    HomePage(),
+    TopGrow(),
+    BookMarks(),
+    ProfileW(),
+  ];
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  List<Text> _titles = [
+    Text(
+      'All Books',
+      style: TextStyle(color: Colors.black),
+    ),
+    Text(
+      'Top Growsing',
+      style: TextStyle(color: Colors.black),
+    ),
+    Text(
+      'Bookmarks',
+      style: TextStyle(color: Colors.black),
+    ),
+    Text(
+      'Profile',
+      style: TextStyle(color: Colors.black),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'All Books',
-          style: TextStyle(color: Colors.black),
-        ),
+        title: _titles[_selectedIndex],
         centerTitle: true,
         leading: Icon(
           LineIcons.navicon,
           color: Colors.black,
           size: 30,
         ),
-        actions: [
-          Padding(
-            padding: EdgeInsets.only(right: 15),
-            child: Icon(Icons.search, color: Colors.black, size: 30),
-          )
-        ],
+        actions: _selectedIndex == 3
+            ? []
+            : [
+                Padding(
+                  padding: EdgeInsets.only(right: 15),
+                  child: Icon(Icons.search, color: Colors.black, size: 30),
+                )
+              ],
       ),
-      bottomNavigationBar: BottomBar(),
-      body: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: [
-              MyBtn(
-                title: 'Ebooks',
-                onp: () {},
-              ),
-              MyBtn(
-                title: 'Most Populer',
-                onp: () {},
-              ),
-              MyBtn(
-                title: 'New',
-                onp: () {},
-              ),
-              MyBtn(
-                title: 'Requested',
-                onp: () {},
-              ),
-            ],
-          ),
-        ),
-        Expanded(
-          flex: 8,
-          child: StaggeredGridView.count(
-            crossAxisCount: 2,
-            children: [
-              Container(
-                height: 300,
-                color: Colors.lightBlue,
-                child: Center(
-                  child: Text('holy'),
-                ),
-              ),
-              Container(
-                height: 350,
-                color: Colors.lightGreen,
-                child: Center(
-                  child: Text('holy'),
-                ),
-              ),
-              Container(
-                height: 300,
-                color: Colors.lightBlue,
-                child: Center(
-                  child: Text('holy'),
-                ),
-              ),
-              Container(
-                height: 350,
-                color: Colors.lightGreen,
-                child: Center(
-                  child: Text('holy'),
-                ),
-              ),
-              Container(
-                height: 300,
-                color: Colors.lightBlue,
-                child: Center(
-                  child: Text('holy'),
-                ),
-              ),
-              Container(
-                height: 350,
-                color: Colors.lightGreen,
-                child: Center(
-                  child: Text('holy'),
-                ),
-              ),
-            ],
-            scrollDirection: Axis.vertical,
-            crossAxisSpacing: 12,
-            mainAxisSpacing: 12,
-            staggeredTiles: [
-              StaggeredTile.fit(1),
-              StaggeredTile.fit(1),
-              StaggeredTile.fit(1),
-              StaggeredTile.fit(1),
-              StaggeredTile.fit(1),
-              StaggeredTile.fit(1),
-            ],
-          ),
-        ),
-      ],
+      bottomNavigationBar: BottomBar(
+        sInd: _selectedIndex,
+        onp: (index) {
+          setState(() {
+            _selectedIndex = index;
+            print(_selectedIndex);
+          });
+        },
+      ),
+      body: MyHomePage.myPages.elementAt(_selectedIndex),
     );
   }
 }

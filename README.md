0de29519c14b84d7cea5f6c73bf91aece02385d7
RomjanHossain <romjanhossain726526@gmail.com>
2020-10-02 15:51:55 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# know Your Book
+
+let's see how far i can go with this project xD
---
+++ android/app/src/main/kotlin/com/example/knowyourbook/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.knowyourbook
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}

54e87313a0467205b582c402648c99bc6f61d5ad
RomjanHossain <romjanhossain726526@gmail.com>
2020-10-02 18:54:40 +0600

    widget folder added and all widget separated

---
+++ lib/Widgets/bottomNavBar.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:line_icons/line_icons.dart';
+
+class BottomBar extends StatefulWidget {
+  @override
+  _BottomBarState createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: Colors.white, boxShadow: [
+        BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.1))
+      ]),
+      child: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+          child: GNav(
+              gap: 8,
+              activeColor: Colors.white,
+              color: Colors.black,
+              iconSize: 25,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              duration: Duration(milliseconds: 800),
+              tabBackgroundColor: Colors.blue,
+              tabs: [
+                GButton(
+                  icon: LineIcons.home,
+                  text: 'Home',
+                ),
+                GButton(
+                  icon: LineIcons.bar_chart_o,
+                  text: 'Top Growsing',
+                ),
+                GButton(
+                  icon: LineIcons.bookmark,
+                  text: 'BookMarks',
+                ),
+                GButton(
+                  icon: LineIcons.user,
+                  text: 'Profile',
+                ),
+              ],
+              selectedIndex: _selectedIndex,
+              onTabChange: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              }),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Widgets/myBtn.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MyBtn extends StatelessWidget {
+  MyBtn({@required this.onp, @required this.title});
+  final String title;
+  final Function onp;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(15)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
+          color: Colors.white,
+        ),
+        width: 200,
+        child: MaterialButton(
+          onPressed: onp,
+          // color: Colors.white,
+          child: Text(title),
+        ),
+      ),
+    );
+  }
+}

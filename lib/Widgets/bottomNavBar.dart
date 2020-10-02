f6af1a06177be6ca066849e185c86f00e7d470d3
RomjanHossain <romjanhossain726526@gmail.com>
2020-10-03 00:01:20 +0600

    bottom nav bar

--- lib/Widgets/bottomNavBar.dart
+++ lib/Widgets/bottomNavBar.dart
@@ -3,12 +3,14 @@ import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:line_icons/line_icons.dart';
 
 class BottomBar extends StatefulWidget {
+  BottomBar({this.onp, this.sInd});
+  final Function onp;
+  final int sInd;
   @override
   _BottomBarState createState() => _BottomBarState();
 }
 
 class _BottomBarState extends State<BottomBar> {
-  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -19,37 +21,34 @@ class _BottomBarState extends State<BottomBar> {
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
           child: GNav(
-              gap: 8,
-              activeColor: Colors.white,
-              color: Colors.black,
-              iconSize: 25,
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-              duration: Duration(milliseconds: 800),
-              tabBackgroundColor: Colors.blue,
-              tabs: [
-                GButton(
-                  icon: LineIcons.home,
-                  text: 'Home',
-                ),
-                GButton(
-                  icon: LineIcons.bar_chart_o,
-                  text: 'Top Growsing',
-                ),
-                GButton(
-                  icon: LineIcons.bookmark,
-                  text: 'BookMarks',
-                ),
-                GButton(
-                  icon: LineIcons.user,
-                  text: 'Profile',
-                ),
-              ],
-              selectedIndex: _selectedIndex,
-              onTabChange: (index) {
-                setState(() {
-                  _selectedIndex = index;
-                });
-              }),
+            gap: 8,
+            activeColor: Colors.white,
+            color: Colors.black,
+            iconSize: 25,
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            duration: Duration(milliseconds: 800),
+            tabBackgroundColor: Colors.blue,
+            tabs: [
+              GButton(
+                icon: LineIcons.home,
+                text: 'Home',
+              ),
+              GButton(
+                icon: LineIcons.bar_chart_o,
+                text: 'Top Growsing',
+              ),
+              GButton(
+                icon: LineIcons.bookmark,
+                text: 'BookMarks',
+              ),
+              GButton(
+                icon: LineIcons.user,
+                text: 'Profile',
+              ),
+            ],
+            selectedIndex: widget.sInd,
+            onTabChange: widget.onp,
+          ),
         ),
       ),
     );

562de25cc7cad585726af36402e1fb801b7ebf41
RomjanHossain <romjanhossain726526@gmail.com>
2020-10-04 00:06:03 +0600

    grid buildcount added

--- lib/Screens/swapTrand/books.dart
+++ lib/Screens/swapTrand/books.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:knowyourbook/Screens/BookView/bookView.dart';
 import 'package:knowyourbook/Widgets/myBtn.dart';
 
 class HomePage extends StatefulWidget {
@@ -66,11 +67,15 @@ class _HomePageState extends State<HomePage> {
     Container(
       height: 300,
       decoration: BoxDecoration(
+          image: DecorationImage(
+            alignment: Alignment.center,
+            fit: BoxFit.cover,
+            image: NetworkImage(
+              'https://i.pinimg.com/originals/62/04/3b/62043b0bb353f49600a23ed1b5ec922f.jpg',
+            ),
+          ),
           color: Colors.lightBlue,
           borderRadius: BorderRadius.all(Radius.circular(20))),
-      child: Center(
-        child: Text('holy'),
-      ),
     ),
   ];
   @override
@@ -115,24 +120,65 @@ class _HomePageState extends State<HomePage> {
             child: TabBarView(
               children: [
                 // new Books
-                StaggeredGridView.count(
+                StaggeredGridView.countBuilder(
+                  itemBuilder: (context, index) {
+                    return Container(
+                      height: 300,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                            alignment: Alignment.center,
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                              'https://i.pinimg.com/originals/62/04/3b/62043b0bb353f49600a23ed1b5ec922f.jpg',
+                            ),
+                          ),
+                          color: Colors.lightBlue,
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      // ),
+                    );
+                  },
+                  itemCount: 10,
+                  staggeredTileBuilder: (index) {
+                    return StaggeredTile.count(1, index.isEven ? 1.5 : 1.8);
+                  },
                   crossAxisCount: 2,
                   padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-                  children: books,
                   scrollDirection: Axis.vertical,
                   crossAxisSpacing: 12,
                   mainAxisSpacing: 12,
-                  staggeredTiles: mystaggeredTiles,
                 ),
                 // most popular
-                StaggeredGridView.count(
-                  crossAxisCount: 3,
+                StaggeredGridView.countBuilder(
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, BookView.id);
+                      },
+                      child: Container(
+                        height: 300,
+                        decoration: BoxDecoration(
+                            color: Colors.lightBlue,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20))),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          child: Image.network(
+                            'https://i.pinimg.com/originals/62/04/3b/62043b0bb353f49600a23ed1b5ec922f.jpg',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: 10,
+                  staggeredTileBuilder: (index) {
+                    return StaggeredTile.count(1, 1.5);
+                  },
+                  crossAxisCount: 2,
                   padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-                  children: books,
                   scrollDirection: Axis.vertical,
                   crossAxisSpacing: 12,
                   mainAxisSpacing: 12,
-                  staggeredTiles: mystaggeredTiles,
                 ),
                 // Tranding
                 StaggeredGridView.count(

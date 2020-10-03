a54c88a3a835c7b649896dfc5844a6557f8773f3
RomjanHossain <romjanhossain726526@gmail.com>
2020-10-03 13:57:42 +0600

    delete unnecessery changes

--- lib/Screens/swapTrand/profile.dart
+++ lib/Screens/swapTrand/profile.dart
@@ -1,134 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:knowyourbook/Widgets/myBtn.dart';
 
 class ProfileW extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: EdgeInsets.all(20),
-          child: Container(
-            decoration: BoxDecoration(
-              // boxShadow: [
-
-              // ],
-              color: Colors.white,
-              borderRadius: BorderRadius.all(Radius.circular(20)),
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      child: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black,
+              blurRadius: 2,
             ),
-            height: MediaQuery.of(context).size.height / 1.8,
-            width: MediaQuery.of(context).size.width,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  backgroundImage: null,
-                  radius: 70,
-                ),
-                ListTile(
-                  title: Center(
-                    child: Text(
-                      'sdjf;lsd',
-                      style: TextStyle(
-                        fontSize: 25,
-                      ),
-                    ),
-                  ),
-                  subtitle: Center(child: Text('fsa;jdklf;asd')),
-                  contentPadding:
-                      EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    SizedBox(
-                      width: 100,
-                      // height: 50,
-                      child: ListTile(
-                        subtitle: Center(child: Text('status')),
-                        title: Center(
-                          child: Text(
-                            'Single',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                        ),
-                        dense: true,
-                        contentPadding: EdgeInsets.all(0),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 100,
-                      // height: 50,
-                      child: ListTile(
-                        title: Center(
-                          child: Text(
-                            'Male',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                        ),
-                        dense: true,
-                        contentPadding: EdgeInsets.all(0),
-                        subtitle: Center(child: Text('Gender')),
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    FloatingActionButton(
-                      onPressed: () async {
-                        // CurdServide().createUserData(user);
-                        // CurdServide().updateUserData(user);
-                      },
-                      child: Icon(Icons.mail),
-                      backgroundColor: Colors.indigoAccent,
-                    ),
-                    FloatingActionButton(
-                      onPressed: null,
-                      child: Text('kl'),
-                    ),
-                    FloatingActionButton(
-                      backgroundColor: Colors.green[300],
-                      onPressed: null,
-                      child: Icon(
-                        Icons.phone,
-                      ),
-                    ),
-                  ],
-                )
-              ],
+            BoxShadow(
+              color: Colors.blue,
             ),
-          ),
+          ],
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(20)),
         ),
-        Expanded(
-          child: ListView(
-            padding: EdgeInsets.all(10),
-            children: [
-              ListTile(
-                title: Text('99 Meadow City'),
-                subtitle: Text('Address'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundImage: null,
+              radius: 70,
+            ),
+            ListTile(
+              title: Center(
+                child: Text(
+                  'Romjan D. Hossain',
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
               ),
-              ListTile(
-                title: Text('1216'),
-                subtitle: Text('Zip Code'),
-              ),
-              ListTile(
-                title: Text('Dhaka'),
-                subtitle: Text('City'),
-              ),
-              ListTile(
-                title: Text('Bangladesh'),
-                subtitle: Text('Country'),
-              ),
-              ListTile(
-                title: Text('99 Meadow City'),
-                subtitle: Text('Address'),
-              ),
-            ],
-          ),
-        )
-      ],
+              subtitle: Center(child: Text('romjan@gmail.com')),
+              contentPadding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+            ),
+            Card(
+              child: Text('My books'),
+            ),
+            Card(
+              child: Text('Bookmarks'),
+            ),
+            Card(
+              child: Text('dont know what to write'),
+            ),
+            MyBtn(
+                onp: () {
+                  print('pressed');
+                },
+                title: 'Log Out'),
+          ],
+        ),
+      ),
     );
   }
 }

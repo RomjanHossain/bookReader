4332712153ec8a07ecaa3c0b8358959bdd0222fe
RomjanHossain <romjanhossain726526@gmail.com>
2020-10-03 13:56:33 +0600

    added another small btn

--- lib/Widgets/myBtn.dart
+++ lib/Widgets/myBtn.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 class MyBtn extends StatelessWidget {
-  MyBtn({@required this.onp, @required this.title});
+  MyBtn({@required this.onp, @required this.title, this.isActive = false});
   final String title;
   final Function onp;
+  final bool isActive;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -13,7 +14,9 @@ class MyBtn extends StatelessWidget {
           borderRadius: BorderRadius.all(Radius.circular(15)),
           boxShadow: [
             BoxShadow(
-              color: Colors.black.withOpacity(0.1),
+              color: isActive
+                  ? Colors.blue.withOpacity(0.5)
+                  : Colors.black.withOpacity(0.1),
               spreadRadius: 2,
               blurRadius: 7,
               offset: Offset(0, 3), // changes position of shadow
@@ -21,12 +24,46 @@ class MyBtn extends StatelessWidget {
           ],
           color: Colors.white,
         ),
-        width: 200,
-        child: MaterialButton(
+        child: RawMaterialButton(
           onPressed: onp,
-          // color: Colors.white,
+          padding: EdgeInsets.symmetric(horizontal: 25),
           child: Text(title),
         ),
+      ),
+    );
+  }
+}
+
+class KSmallBtn extends StatelessWidget {
+  KSmallBtn({@required this.iconData, @required this.text});
+  final IconData iconData;
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 15,
+            offset: Offset(2, 2), // changes position of shadow
+          ),
+        ],
+        color: Colors.white,
+      ),
+      child: MaterialButton(
+        onPressed: () {
+          print('clicked');
+        },
+        child: Row(
+          children: [
+            Icon(iconData),
+            Text(text),
+          ],
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 25),
       ),
     );
   }

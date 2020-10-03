cdd5c5462d8535c0b11aa96cd4768b1ae1a964e9
RomjanHossain <romjanhossain726526@gmail.com>
2020-10-03 13:56:07 +0600

    adding const

---
+++ lib/values/const.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+// const kBoxNumberbig = TextStyle();
+// const kBoxNumbersmall =  Card(
+
+// );
+const kTitle = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w400,
+  fontSize: 20,
+);
+
+const kSubtitle = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w300,
+  fontSize: 16,
+  letterSpacing: 2,
+);

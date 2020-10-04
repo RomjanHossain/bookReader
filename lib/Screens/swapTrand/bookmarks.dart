4c9e80d17cbdeca1b8a6f968355e2f5012fe44ce
RomjanHossain <romjanhossain726526@gmail.com>
2020-10-04 15:35:44 +0600

    updated

--- lib/Screens/swapTrand/bookmarks.dart
+++ lib/Screens/swapTrand/bookmarks.dart
@@ -1,12 +1,85 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
-class BookMarks extends StatelessWidget {
+class BookMarks extends StatefulWidget {
+  @override
+  _BookMarksState createState() => _BookMarksState();
+}
+
+class _BookMarksState extends State<BookMarks> {
+  String dropdownValue = 'name';
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: Colors.purple,
-      ),
+    return Column(
+      children: [
+        Expanded(
+          child: Align(
+            alignment: Alignment.topRight,
+            child: DropdownButton<String>(
+              value: dropdownValue,
+              iconSize: 24,
+              elevation: 16,
+              style: TextStyle(color: Colors.deepPurple),
+              onChanged: (String newValue) {
+                setState(() {
+                  dropdownValue = newValue;
+                });
+              },
+              items: <String>['name', 'Two', 'Free', 'Four']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 9,
+          child: StaggeredGridView.countBuilder(
+            itemBuilder: (context, index) {
+              return Container(
+                height: 300,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    alignment: Alignment.center,
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                      'https://i.pinimg.com/originals/62/04/3b/62043b0bb353f49600a23ed1b5ec922f.jpg',
+                    ),
+                  ),
+                  color: Colors.lightBlue,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                ),
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: Padding(
+                    padding: EdgeInsets.only(right: 8, top: 5),
+                    child: Icon(
+                      CupertinoIcons.bookmark,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              );
+            },
+            itemCount: 12,
+            staggeredTileBuilder: (index) {
+              return StaggeredTile.count(1, index.isEven ? 1.5 : 1.7);
+            },
+            crossAxisCount: 3,
+            padding: EdgeInsets.symmetric(horizontal: 5, vertical: 15),
+            scrollDirection: Axis.vertical,
+            crossAxisSpacing: 12,
+            mainAxisSpacing: 12,
+          ),
+        ),
+      ],
     );
   }
 }

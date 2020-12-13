da30aa1243f527ec526fd370882e38661879bbaf
RomjanHossain <romjanhossain726526@gmail.com>
2020-12-14 00:11:50 +0600

    facebook auth del

--- lib/services/firebase/auth.dart
+++ lib/services/firebase/auth.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:async';
 
@@ -36,12 +35,12 @@ class AuthServices {
       createUserData(userCredential.user);
       return userCredential.user;
     } catch (e) {
+      print('error from Google Sign in method:\n$e');
       return null;
     }
   }
 
   Future<void> createUserData(User user) {
-    // nums = nums + 1;
     DocumentReference reportRef = _db.collection('reports').doc(user.uid);
     return reportRef.set(
       {
@@ -57,32 +56,8 @@ class AuthServices {
     );
   }
 
-  Future<String> getProfilePic() async {
-    return _auth.currentUser.photoURL;
-  }
-
-  //? facebook sign in!
-  Future<User> signInWithFacebook() async {
-    // Trigger the sign-in flow
-    final result = await FacebookAuth.instance.login();
-
-    // Create a credential from the access token
-    final FacebookAuthCredential facebookAuthCredential =
-        FacebookAuthProvider.credential(result.token);
-
-    // Once signed in, return the UserCredential
-    UserCredential usercredential = await FirebaseAuth.instance
-        .signInWithCredential(facebookAuthCredential);
-    createUserData(usercredential.user);
-    return usercredential.user;
-  }
-
   //! signout
   Future<void> signOut() async {
     return await _auth.signOut();
-  }
-
-  Future<void> logOut() async {
-    return await FacebookAuth.instance.logOut();
   }
 }

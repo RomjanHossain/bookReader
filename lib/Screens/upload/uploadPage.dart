fb2ec75ce6ce4301c5e6ca96990fda672533988a
RomjanHossain <romjanhossain726526@gmail.com>
2020-12-19 00:10:31 +0600

    d

--- lib/Screens/upload/uploadPage.dart
+++ lib/Screens/upload/uploadPage.dart
@@ -6,6 +6,8 @@ import 'package:knowyourbook/services/firebase/auth.dart';
 import 'package:knowyourbook/services/firebase/storage.dart';
 import 'package:provider/provider.dart';
 
+enum tags { harder, smarter, selfStarter, tradingCharter }
+
 class UpLoadPage extends StatefulWidget {
   static const String id = 'upload';
 
@@ -14,8 +16,155 @@ class UpLoadPage extends StatefulWidget {
 }
 
 class _UpLoadPageState extends State<UpLoadPage> {
+  List<Widget> _listofCat = [
+    Text('Tags'),
+    // FloatingActionButton(
+    //   onPressed: () {
+
+    //       _listofCat.add(Text('skdjfl'));
+
+    //   },
+    //   mini: true,
+    //   elevation: 3,
+    //   child: Icon(Icons.add),
+    // ),
+  ];
   final _globalKey = GlobalKey<ScaffoldState>();
+  final _formKey = GlobalKey<FormState>();
   bool _isUploading = false;
+  void displayBottomSheet(BuildContext context, User user) {
+    showGeneralDialog(
+      barrierLabel: '',
+      barrierDismissible: true,
+      pageBuilder: (ctx, anim1, anim2) => AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
+        ),
+        scrollable: true,
+        title: Center(
+            child: Center(
+          child: Text('Upload your Book'),
+        )),
+        content: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              //? name field
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: TextFormField(
+                  autocorrect: false,
+                  decoration: InputDecoration(
+                    labelText: 'Name',
+                    border: InputBorder.none,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    hintText: 'Romjan Hossain',
+                  ),
+                  textCapitalization: TextCapitalization.words,
+                ),
+              ),
+              //? description field
+              TextFormField(
+                autocorrect: false,
+                decoration: InputDecoration(
+                  labelText: 'Description',
+                  border: InputBorder.none,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                maxLines: 10,
+                textAlign: TextAlign.center,
+                textCapitalization: TextCapitalization.sentences,
+              ),
+              SingleChildScrollView(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: _listofCat,
+                ),
+              ),
+              GestureDetector(
+                onTap: () async {
+                  File _file = await Provider.of<FirebaseStorageService>(
+                          context,
+                          listen: false)
+                      .pickFile();
+                  if (_file != null) {
+                    //? upload it to firebase storage
+                    Navigator.pop(context);
+                    // LinearProgressIndicator(
+                    //   key: _globalKey,
+                    // );
+                    setState(() {
+                      _isUploading = true;
+                    });
+                    await Provider.of<FirebaseStorageService>(context,
+                            listen: false)
+                        .uploadFile(_file, user)
+                        .then((String bookid) {
+                      Provider.of<AuthServices>(context, listen: false)
+                          .userUploaded(bookid, user);
+                      var snackBar = _ownBar('Uploaded', Colors.green);
+                      _globalKey.currentState.showSnackBar(snackBar);
+                      setState(() {
+                        _isUploading = false;
+                      });
+                    });
+                  } else {
+                    //? user cancelled
+                    Navigator.pop(context);
+                    var snackBar = _ownBar('Canceled', Colors.red);
+                    _globalKey.currentState.showSnackBar(snackBar);
+                  }
+                },
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    border: Border.all(
+                      color: Colors.blue,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  child: SizedBox(
+                    height: 30,
+                    width: MediaQuery.of(context).size.width / 1.2,
+                    child:
+                        Center(child: Image.asset('assets/images/epub2.png')),
+                  ),
+                ),
+              ),
+              FlatButton(
+                color: Colors.blue,
+                colorBrightness: Brightness.dark,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                onPressed: () {},
+                child: Text('Submit'),
+              ),
+            ],
+          ),
+        ),
+        elevation: 2,
+      ),
+      transitionBuilder: (ctx, anim1, anim2, child) => BackdropFilter(
+        filter:
+            ImageFilter.blur(sigmaX: 4 * anim1.value, sigmaY: 4 * anim1.value),
+        child: FadeTransition(
+          child: child,
+          opacity: anim1,
+        ),
+      ),
+      context: context,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,90 +174,7 @@ class _UpLoadPageState extends State<UpLoadPage> {
       key: _globalKey,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          showGeneralDialog(
-            barrierDismissible: true,
-            barrierLabel: '',
-            barrierColor: Colors.black38,
-            pageBuilder: (ctx, anim1, anim2) => AlertDialog(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(30))),
-              scrollable: true,
-              title: Center(
-                  child: Center(
-                child: Text('Upload your Epub file'),
-              )),
-              content: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GestureDetector(
-                    onTap: () async {
-                      File _file = await Provider.of<FirebaseStorageService>(
-                              context,
-                              listen: false)
-                          .pickFile();
-                      if (_file != null) {
-                        //? upload it to firebase storage
-                        Navigator.pop(context);
-                        // LinearProgressIndicator(
-                        //   key: _globalKey,
-                        // );
-                        setState(() {
-                          _isUploading = true;
-                        });
-                        await Provider.of<FirebaseStorageService>(context,
-                                listen: false)
-                            .uploadFile(_file, user)
-                            .then((String bookid) {
-                          Provider.of<AuthServices>(context, listen: false)
-                              .userUploaded(bookid, user);
-                          var snackBar = _ownBar('Uploaded', Colors.green);
-                          _globalKey.currentState.showSnackBar(snackBar);
-                          setState(() {
-                            _isUploading = false;
-                          });
-                        });
-                      } else {
-                        //? user cancelled
-                        Navigator.pop(context);
-                        var snackBar = _ownBar('Canceled', Colors.red);
-                        _globalKey.currentState.showSnackBar(snackBar);
-                      }
-                    },
-                    child: Container(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 80,
-                      ),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        border: Border.all(
-                          color: Colors.blue,
-                          style: BorderStyle.solid,
-                        ),
-                      ),
-                      child: SizedBox(
-                        height: 60,
-                        width: MediaQuery.of(context).size.width / 1.2,
-                        child: Center(
-                            child: Image.asset('assets/images/epub2.png')),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              elevation: 2,
-            ),
-            transitionBuilder: (ctx, anim1, anim2, child) => BackdropFilter(
-              filter: ImageFilter.blur(
-                  sigmaX: 4 * anim1.value, sigmaY: 4 * anim1.value),
-              child: FadeTransition(
-                child: child,
-                opacity: anim1,
-              ),
-            ),
-            context: context,
-          );
-
+          displayBottomSheet(context, user);
           //! showend
         },
         child: Icon(Icons.add),

483181e1ec1ed42fec9338bb32d52d680e6fd0d7
RomjanHossain <romjanhossain726526@gmail.com>
2020-12-23 23:28:02 +0600

    catagory and snackbar and upload filealert added

--- lib/values/const.dart
+++ lib/values/const.dart
@@ -26,3 +26,187 @@ Random _rnd = Random();
 
 String getRandomString(int length) => String.fromCharCodes(Iterable.generate(
     length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
+
+//? catagory
+enum Category {
+  Action,
+  Adventure,
+  Classics,
+  Detective,
+  Mystery,
+  Fantasy,
+  Historical,
+  Horror,
+  Literacy,
+  Romance,
+  Short,
+  Suspense,
+  Thrillers,
+  Women,
+  Adult
+}
+//? snak bar
+SnackBar kownBar(String txt, Color col) {
+  return SnackBar(
+    backgroundColor: col,
+    behavior: SnackBarBehavior.floating,
+    duration: Duration(seconds: 1),
+    content: Text(
+      txt,
+      style: TextStyle(color: Colors.black, fontSize: 20),
+      textAlign: TextAlign.center,
+    ),
+  );
+}
+//! upload method
+/*
+  void displayBottomSheet(BuildContext context, User user) {
+    showGeneralDialog(
+      barrierLabel: '',
+      barrierDismissible: true,
+      pageBuilder: (ctx, anim1, anim2) => AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
+        ),
+        scrollable: true,
+        title: Center(
+            child: Center(
+          child: Text('Upload your Book'),
+        )),
+        content: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              //? name field
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: TextFormField(
+                  autocorrect: false,
+                  decoration: InputDecoration(
+                    labelText: 'Name',
+                    border: InputBorder.none,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    hintText: 'Romjan Hossain',
+                  ),
+                  textCapitalization: TextCapitalization.words,
+                ),
+              ),
+              //? description field
+              TextFormField(
+                autocorrect: false,
+                decoration: InputDecoration(
+                  labelText: 'Description',
+                  border: InputBorder.none,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                maxLines: 10,
+                textAlign: TextAlign.center,
+                textCapitalization: TextCapitalization.sentences,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  SingleChildScrollView(
+                    physics: BouncingScrollPhysics(),
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: _listofCat,
+                    ),
+                  ),
+                  RawMaterialButton(
+                    onPressed: () {
+                      setState(() {
+                        _listofCat.add(Text('skdjfl'));
+                      });
+                    },
+                    fillColor: Colors.white.withOpacity(.7),
+                    child: Icon(Icons.add),
+                    shape: CircleBorder(),
+                  ),
+                ],
+              ),
+              GestureDetector(
+                onTap: () async {
+                  File _file = await Provider.of<FirebaseStorageService>(
+                          context,
+                          listen: false)
+                      .pickFile();
+                  if (_file != null) {
+                    //? upload it to firebase storage
+                    Navigator.pop(context);
+                    // LinearProgressIndicator(
+                    //   key: _globalKey,
+                    // );
+                    setState(() {
+                      _isUploading = true;
+                    });
+                    await Provider.of<FirebaseStorageService>(context,
+                            listen: false)
+                        .uploadFile(_file, user)
+                        .then((String bookid) {
+                      Provider.of<AuthServices>(context, listen: false)
+                          .userUploaded(bookid, user);
+                      var snackBar = _ownBar('Uploaded', Colors.green);
+                      _globalKey.currentState.showSnackBar(snackBar);
+                      setState(() {
+                        _isUploading = false;
+                      });
+                    });
+                  } else {
+                    //? user cancelled
+                    Navigator.pop(context);
+                    var snackBar = _ownBar('Canceled', Colors.red);
+                    _globalKey.currentState.showSnackBar(snackBar);
+                  }
+                },
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    border: Border.all(
+                      color: Colors.blue,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  child: SizedBox(
+                    height: 30,
+                    width: MediaQuery.of(context).size.width / 1.2,
+                    child:
+                        Center(child: Image.asset('assets/images/epub2.png')),
+                  ),
+                ),
+              ),
+              FlatButton(
+                color: Colors.blue,
+                colorBrightness: Brightness.dark,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                onPressed: () {},
+                child: Text('Submit'),
+              ),
+            ],
+          ),
+        ),
+        elevation: 2,
+      ),
+      transitionBuilder: (ctx, anim1, anim2, child) => BackdropFilter(
+        filter:
+            ImageFilter.blur(sigmaX: 4 * anim1.value, sigmaY: 4 * anim1.value),
+        child: FadeTransition(
+          child: child,
+          opacity: anim1,
+        ),
+      ),
+      context: context,
+    );
+  }
+
+  */

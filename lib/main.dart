f107cd041ada73188e085358c9fc5435945bfa44
RomjanHossain <romjanhossain726526@gmail.com>
2020-12-27 15:56:44 +0600

    removing save provider

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'package:knowyourbook/Screens/upload/uploadPage.dart';
 import 'package:knowyourbook/services/firebase/auth.dart';
 import 'package:knowyourbook/services/firebase/database.dart';
 import 'package:knowyourbook/services/firebase/storage.dart';
-import 'package:knowyourbook/services/saveBook/savebookProv.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
@@ -27,7 +26,6 @@ class MyApp extends StatelessWidget {
         Provider(create: (context) => AuthServices()),
         Provider(create: (context) => FirebaseStorageService()),
         Provider(create: (context) => DatabaseService()),
-        Provider(create: (context) => SaveBookProvider()),
       ],
       child: MaterialApp(
         title: 'Flutter Demo',

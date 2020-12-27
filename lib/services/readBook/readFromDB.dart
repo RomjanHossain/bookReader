a89e9c396f4c11baaa6971993ee28a4e88340b0d
RomjanHossain <romjanhossain726526@gmail.com>
2020-12-27 15:59:59 +0600

    save lastlocation of book to user storage

--- lib/services/readBook/readFromDB.dart
+++ lib/services/readBook/readFromDB.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
 import 'package:epub_viewer/epub_viewer.dart';
+import '../saveBook/savebook.dart';
 
 class ReadFromDB {
   //! first you have to save the book
   //? save the book on chache
   //? save the book on memory
   //! then you have to read the book!!!
-  openBook(String url) async {
+  openBook(String url, String bookid) async {
+    List locators = await LocatorDB().getLocator(bookid);
     EpubViewer.setConfig(
       identifier: "Book",
       scrollDirection: EpubScrollDirection.ALLDIRECTIONS,
@@ -17,18 +19,16 @@ class ReadFromDB {
 
     EpubViewer.open(
       '$url',
-      lastLocation: EpubLocator.fromJson(
-        {
-          "bookId": "2239",
-          "href": "/OEBPS/ch06.xhtml",
-          "created": 1539934158390,
-          "locations": {"cfi": "epubcfi(/0!/4/4[simple_book]/2/2/6)"}
-        },
-      ),
+      lastLocation:
+          locators.isNotEmpty ? EpubLocator.fromJson(locators[0]) : null,
     );
     // get current locator
-    EpubViewer.locatorStream.listen((locator) {
-      print('LOCATOR: ${EpubLocator.fromJson(jsonDecode(locator))}');
+    EpubViewer.locatorStream.listen((event) async {
+      // Get locator here
+      Map json = jsonDecode(event);
+      json['bookId'] = bookid;
+      // Save locator to your database
+      await LocatorDB().update(json);
     });
   }
 }

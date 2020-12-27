120cdc429266e015d76b00bb071146476e976409
RomjanHossain <romjanhossain726526@gmail.com>
2020-12-27 16:02:37 +0600

    save lastlocation of book to user storage class

--- lib/services/saveBook/savebook.dart
+++ lib/services/saveBook/savebook.dart
@@ -1,87 +1,54 @@
-// import 'dart:io';
-// import 'package:dio/dio.dart';
-// import 'package:path_provider/path_provider.dart';
-// import 'package:permission_handler/permission_handler.dart';
+import 'dart:io';
 
-// class DownloadFile {
-//   final Dio dio = Dio();
-//   bool loading = false;
-//   double progress = 0;
+import 'package:objectdb/objectdb.dart';
+import 'package:path_provider/path_provider.dart';
 
-//   //? save the file
-//   Future<bool> saveEpub(String url, String fileName) async {
-//     Directory directory;
-//     try {
-//       if (Platform.isAndroid) {
-//         if (await _requestPermission(Permission.storage)) {
-//           directory = await getExternalStorageDirectory();
-//           String newPath = "";
-//           print(directory);
-//           List<String> paths = directory.path.split("/");
-//           for (int x = 1; x < paths.length; x++) {
-//             String folder = paths[x];
-//             if (folder != "Android") {
-//               newPath += "/" + folder;
-//             } else {
-//               break;
-//             }
-//           }
-//           newPath = newPath + "/RPSApp";
-//           directory = Directory(newPath);
-//         } else {
-//           return false;
-//         }
-//       }
-//       File saveFile = File(directory.path + "/$fileName");
-//       if (!await directory.exists()) {
-//         await directory.create(recursive: true);
-//       }
-//       if (await directory.exists()) {
-//         await dio.download(url, saveFile.path,
-//             onReceiveProgress: (value1, value2) {
-//           setState(() {
-//             progress = value1 / value2;
-//           });
-//         });
+class LocatorDB {
+  getPath() async {
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    final path = documentDirectory.path + '/locator.db';
+    return path;
+  }
 
-//         return true;
-//       }
-//       return false;
-//     } catch (e) {
-//       print(e);
-//       return false;
-//     }
-//   }
+  //Insertion
+  add(Map item) async {
+    final db = ObjectDB(await getPath());
+    db.open();
+    db.insert(item);
+    await db.close();
+  }
 
-//   //? handle permission
-//   Future<bool> _requestPermission(Permission permission) async {
-//     if (await permission.isGranted) {
-//       return true;
-//     } else {
-//       var result = await permission.request();
-//       if (result == PermissionStatus.granted) {
-//         return true;
-//       }
-//     }
-//     return false;
-//   }
+  update(Map item) async {
+    final db = ObjectDB(await getPath());
+    db.open();
+    int update = await db.update({'bookId': item['bookId']}, item);
+    if (update == 0) {
+      db.insert(item);
+    }
+    await db.close();
+  }
 
-//   //? download the file!!!
-//   downloadFile() async {
-//     setState(() {
-//       loading = true;
-//       progress = 0;
-//     });
-//     bool downloaded = await saveEpub(
-//         "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4",
-//         "video.mp4");
-//     if (downloaded) {
-//       print("File Downloaded");
-//     } else {
-//       print("Problem Downloading File");
-//     }
-//     setState(() {
-//       loading = false;
-//     });
-//   }
-// }
+  Future<int> remove(Map item) async {
+    final db = ObjectDB(await getPath());
+    db.open();
+    int val = await db.remove(item);
+    await db.close();
+    return val;
+  }
+
+  Future<List> listAll() async {
+    final db = ObjectDB(await getPath());
+    db.open();
+    List val = await db.find({});
+    await db.close();
+    return val;
+  }
+
+  Future<List> getLocator(String id) async {
+    final db = ObjectDB(await getPath());
+    db.open();
+    List val = await db.find({'bookId': id});
+    await db.close();
+    return val;
+  }
+}

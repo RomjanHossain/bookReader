9f7430831d41f6c6ab3fd4eeb0db78ef09dd78df
RomjanHossain <romjanhossain726526@gmail.com>
2020-12-29 12:36:10 +0600

    profile pkg

--- lib/Screens/swapTrand/Profile/profile.dart
+++ lib/Screens/swapTrand/Profile/profile.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'package:knowyourbook/Screens/ordered/orderPage.dart';
+import 'package:knowyourbook/Screens/swapTrand/Profile/components/notloggedin.dart';
 import 'package:knowyourbook/Screens/upload/uploadPage.dart';
 import 'package:knowyourbook/Widgets/myBtn.dart';
 import 'package:knowyourbook/services/firebase/auth.dart';
@@ -110,53 +111,7 @@ class _ProfileWState extends State<ProfileW> {
                   ],
                 ),
               )
-            : Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Column(
-                      children: [
-                        CircleAvatar(
-                          radius: 90,
-                          backgroundImage: AssetImage(
-                            'assets/images/SadFace.png',
-                          ),
-                        ),
-                        Text(
-                          '"Oh-Uh"',
-                          style: TextStyle(
-                            fontSize: 50,
-                          ),
-                        ),
-                        Text(
-                          'Sorry, could not find any account!',
-                          style: TextStyle(
-                            fontSize: 20,
-                          ),
-                        ),
-                        Text(
-                          'Please, add your account',
-                          style: TextStyle(
-                            fontSize: 20,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 100,
-                        ),
-                      ],
-                    ),
-                    MyBtn(
-                      title: 'Log In',
-                      swapActive: true,
-                      toggleSwap: () async {
-                        print('pressed log in');
-                        await Provider.of<AuthServices>(context, listen: false)
-                            .googleSignIn();
-                      },
-                    ),
-                  ],
-                ),
-              ),
+            : NotLoggedIn(),
       ),
     );
   }

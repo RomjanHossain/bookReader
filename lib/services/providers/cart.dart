e44eda43382f3b0e45cda165efd36641800101f8
RomjanHossain <romjanhossain726526@gmail.com>
2020-12-31 00:11:41 +0600

    comment deleted

--- lib/services/providers/cart.dart
+++ lib/services/providers/cart.dart
@@ -1,38 +1,26 @@
 import 'package:flutter/foundation.dart';
+import 'package:knowyourbook/Models/book/bookmod.dart';
 
 class CartModel extends ChangeNotifier {
-  /// Internal, private state of the cart. Stores the ids of each item.
-  final List<int> _itemIds = [];
+  final List<BookModel> cartList = [];
+  int totalBook = 0;
+  int get totalBookPrice {
+    int _pp = 0;
+    cartList.forEach((element) {
+      _pp += element.price;
+    });
+    return _pp;
+  }
 
-  /// The current catalog. Used to construct items from numeric ids.
-
-  /// List of items in the cart.
-  // List<BookModel> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
-
-  /// The current total price of all items.
-  // int get totalPrice =>
-  //     items.fold(0, (total, current) => total + current.price);
-
-  /// Adds [item] to cart. This is the only way to modify the cart from outside.
   void add(BookModel item) {
-    // _itemIds.add(item);
-    // This line tells [Model] that it should rebuild the widgets that
-    // depend on it.
+    cartList.add(item);
+    totalBook += 1;
     notifyListeners();
   }
 
   void remove(BookModel item) {
-    // _itemIds.remove(item.id);
-    // Don't forget to tell dependent widgets to rebuild _every time_
-    // you change the model.
+    cartList.remove(item);
+    totalBook -= 1;
     notifyListeners();
   }
-}
-
-class BookModel {
-  BookModel(
-      {@required this.name, @required this.price, @required this.numofItem});
-  final String name;
-  final int price;
-  final numofItem;
 }

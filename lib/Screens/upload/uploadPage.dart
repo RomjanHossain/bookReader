569c534b0c60ece4349f730d8c7158812361f4b5
RomjanHossain <romjanhossain726526@gmail.com>
2021-01-01 12:43:21 +0600

    demo uploadpage

--- lib/Screens/upload/uploadPage.dart
+++ lib/Screens/upload/uploadPage.dart
@@ -1,11 +1,15 @@
 import 'dart:ui';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:knowyourbook/Screens/upload/uploadForm.dart';
+import 'package:knowyourbook/values/const.dart';
 
 enum tags { harder, smarter, selfStarter, tradingCharter }
 
 class UpLoadPage extends StatelessWidget {
   static const String id = 'upload';
+  UpLoadPage({this.uid});
+  final String uid;
 
   @override
   Widget build(BuildContext context) {
@@ -34,19 +38,56 @@ class UpLoadPage extends StatelessWidget {
           },
         ),
       ),
-      body: ListView(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(
-              vertical: 20,
-            ),
-            child: Text(
-              'Empty',
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ],
-      ),
+      body: FutureBuilder(
+          future: FirebaseFirestore.instance
+              .collection('Users')
+              .doc(uid)
+              .collection('Uploaded')
+              .get(),
+          builder: (context, snapshot) {
+            return snapshot.hasData
+                ? ListView.builder(
+                    physics: BouncingScrollPhysics(),
+                    itemCount: snapshot.data.docs.length,
+                    itemBuilder: (_, i) {
+                      return Column(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.symmetric(
+                                horizontal: 30, vertical: 10),
+                            height: 400,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                alignment: Alignment.center,
+                                fit: BoxFit.cover,
+                                image: AssetImage('images/pro.jpg'),
+                              ),
+                              color: Colors.lightBlue,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(20),
+                              ),
+                            ),
+                          ),
+                          RichText(
+                            overflow: TextOverflow.ellipsis,
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: '${snapshot.data.docs[i]["bookid"]}',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  )
+                : kloading();
+          }),
     );
   }
 }
